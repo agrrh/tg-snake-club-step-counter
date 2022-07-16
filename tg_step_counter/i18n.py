b76ef3d79200af58dbcd345e31dfc8c7ad0a5853
Kirill K <kovalev.kirill.a@gmail.com>
2022-07-16 12:07:25 +0300

    feat: add i18n

---
+++ tg_step_counter/i18n.py
@@ -0,0 +1,30 @@
+import logging
+import yaml
+
+from box import Box
+
+
+class Internationalization(object):
+    MAPS_PATH = "./i18n"
+
+    def __init__(self, lang="en"):
+        self.lang = self.lang
+
+        self.__lang_map_load()
+
+    def __lang_map_load(self):
+        file_path = f"{self.MAPS_PATH}/{self.language}.yml"
+
+        try:
+            fp = open(file_path, "r")
+        except Exception as e:
+            logging.error(f"Could not load file {file_path}: {e}")
+
+        try:
+            lang_data = yaml.load(fp)
+        except Exception as e:
+            logging.error(f"Could not parse YAML from {file_path}: {e}")
+        finally:
+            fp.close()
+
+        self.lang_map = Box(lang_data)

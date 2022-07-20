69d73a19cdabac57ddad70ff7256116b3fb4051d
Kirill K <agrrh@users.noreply.github.com>
2022-07-20 20:57:55 +0300

    feat: use queue based architecture (#3)
    * fix: container name
    
    * feat: add producer
    
    * refactor: infinite
    
    * fix: async
    
    * fix: default nats url
    
    * fix: async
    
    * fix: more async stuff
    
    * fix: actualize code
    
    * fix: nats address
    
    * feat: separate subjects
    
    * refactor: producer renamed to router
    
    * fix: first argument must be callable or None
    
    * chore: debug
    
    * feat: add common consumer
    
    * refactor: rename common to consumer_common
    
    * refactor: rename router to app_router
    
    * fix: get command
    
    * chore: enable common consumer
    
    * chore: debug
    
    * fix: logging
    
    * fix: route finding
    
    * fix: logging
    
    * fix: route selection
    
    * chore: debug
    
    * style: sort imports
    
    * fix: get command from message
    
    * fix: get text from message
    
    * feat: parametrize nats_subject
    
    * fix: message decoding
    
    * fix: main loop
    
    * fix: router name
    
    * fix: message decoding
    
    * style: empty line
    
    * feat: reply to common messages
    
    * style: empty line
    
    * feat: reply to help command
    
    * fix: consumer name
    
    * feat: add help reply
    
    * fix: env vars
    
    * chore: rename
    
    * feat: add consumer-stats
    
    * fix: send photo method
    
    * fix: names
    
    * fix: timeouts
    
    * feat: add consumer result
    
    * feat: router to accept results
    
    * fix: name
    
    * fix: wrong reply
    
    * chore: remove webhook component
    
    * chore: bump to 0.2.0
    
    * fix: use specific nats
    
    * chore: bump prod to 0.2.0

---
+++ app_consumer_stats.py
@@ -0,0 +1,80 @@
+import gspread
+import logging
+import nats
+import os
+import pickle
+
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
+from tg_step_counter.i18n import Internationalization as I18n
+
+from tg_step_counter.objects.result import Result, ResultPlot
+from tg_step_counter.objects.tg_user import TGUser, TGUserSpreadsheetHandler
+
+
+nats_address = os.environ.get("APP_NATS_ADDRESS", "nats://nats.nats.svc:4222")
+nats_subject = os.environ.get("APP_NATS_SUBJECT", "stats.>")
+
+bot_token = os.environ.get("APP_TG_TOKEN")
+bot = AsyncTeleBot(bot_token, parse_mode="Markdown")
+
+google_service_account_fname = os.environ.get("APP_GOOGLE_SA_PATH", "./config/google-service-account.json")
+google_sheet_uri = os.environ.get("APP_GOOGLE_SHEET_URI")
+
+app_language = os.environ.get("APP_LANG", "en")
+i18n = I18n(lang=app_language)
+
+
+async def handler(message, sheet):
+    logging.warning(f"Received a message on: {message.subject}")
+    data = pickle.loads(message.data)
+
+    logging.debug(data)
+
+    result_dummy = Result(date_notation=None)
+
+    tg_user = TGUser(id=data.from_user.id, username=data.from_user.username)
+    tg_user_handler = TGUserSpreadsheetHandler(sheet, tg_user)
+
+    monthly_map = tg_user_handler.get_monthly_map(result_dummy.month)
+    monthly_sum = sum(monthly_map.values())
+
+    result_plot = ResultPlot()
+    plot = result_plot.my_stat(monthly_map)
+    fname = result_plot.save(plot, fname=str(data.from_user.id))
+
+    with open(fname, "rb") as fp:
+        await bot.send_photo(
+            chat_id=data.json.get("chat").get("id"),
+            photo=fp,
+            caption="{webhook_results_monthly}".format(**i18n.lang_map).format(**{"monthly_sum": monthly_sum}),
+            reply_to_message_id=data.id,
+        )
+
+
+async def main():
+    logging.warning(f"Connecting to NATS at: {nats_address}")
+    nc = await nats.connect(nats_address)
+
+    logging.warning(f"Getting updates for subject: {nats_subject}")
+    sub = await nc.subscribe(nats_subject)
+
+    logging.warning(f"Getting Google Spreadsheet: {google_sheet_uri}")
+    gc = gspread.service_account(filename=google_service_account_fname)
+    sheet = gc.open_by_url(google_sheet_uri).sheet1
+
+    try:
+        async for message in sub.messages:
+            await handler(message, sheet)
+    except Exception as e:
+        logging.error(f"Error during message handling: {e}")
+
+    await sub.unsubscribe()
+    await nc.drain()
+
+
+if __name__ == "__main__":
+    logging.critical("Starting consumer/stats")
+
+    asyncio.run(main())
---
+++ app_router.py
@@ -0,0 +1,93 @@
+import logging
+import nats
+import os
+import pickle
+import time
+
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
+from tg_step_counter.i18n import Internationalization as I18n
+
+
+nats_address = os.environ.get("APP_NATS_ADDRESS", "nats://nats.nats.svc:4222")
+
+bot_username = os.environ.get("APP_TG_USERNAME", "step_counter_dev_bot")
+
+bot_token = os.environ.get("APP_TG_TOKEN")
+bot = AsyncTeleBot(bot_token, parse_mode="Markdown")
+
+app_language = os.environ.get("APP_LANG", "en")
+i18n = I18n(lang=app_language)
+
+# fmt: off
+SUBJECT_PREFIXES = {
+    "start": "common",
+    "help": "common",
+    "me": "stats"
+}
+# fmt: on
+
+
+def filter_results_reply(message):
+    if message.reply_to_message is None:
+        return False
+
+    if message.reply_to_message.json.get("text") is None:
+        return False
+
+    proper_user = message.reply_to_message.json.get("from").get("username") == bot_username
+    proper_message = i18n.lang_map.reminder_mark in message.reply_to_message.json.get("text")
+
+    return proper_user and proper_message
+
+
+async def main():
+    logging.warning(f"Connecting to NATS at {nats_address}")
+    nc = await nats.connect(nats_address)
+
+    @bot.message_handler(func=filter_results_reply)
+    async def process_result(message):
+        logging.warning(f"Processing message from {message.chat.id}")
+
+        logging.warning(message)
+        data = pickle.dumps(message)
+
+        subject = f"result.{message.chat.id}"
+
+        logging.warning(f"Sending message to subject {subject}")
+        await nc.publish(subject, data)
+
+    @bot.message_handler(func=lambda x: True)
+    async def process_everything(message):
+        logging.warning(f"Processing message from {message.chat.id}")
+
+        logging.warning(message)
+        data = pickle.dumps(message)
+
+        command = f"not-a-command-{time.time()}"
+
+        message_text = message.json.get("text")
+        if message_text.startswith("/"):
+            try:
+                command = message_text.strip("/").split("@").pop(0)
+            except Exception as e:
+                logging.error(f"Could not get command from message text: {message_text}")
+                logging.error(e)
+
+        logging.warning(command)
+
+        subject_prefix = SUBJECT_PREFIXES.get(command, "null")
+        subject = f"{subject_prefix}.{message.chat.id}"
+
+        logging.warning(f"Sending message to subject {subject}")
+        await nc.publish(subject, data)
+
+    logging.warning("Getting updates")
+    await bot.infinity_polling(timeout=60)
+
+
+if __name__ == "__main__":
+    logging.critical("Starting router")
+
+    asyncio.run(main())

f2b2b1dde48200ab3bed595ee8e3c7e9ca777b9e
Kirill K <kovalev.kirill.a@gmail.com>
2022-07-21 23:29:20 +0300

    chore: test

--- app_consumer_result.py
+++ app_consumer_result.py
@@ -87,15 +87,10 @@ async def main():
     # await sub.unsubscribe()
     # await nc.drain()
 
-    # await asyncio.sleep(15)
+    await asyncio.sleep(15)
 
 
 if __name__ == "__main__":
     logging.critical("Starting consumer/result")
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    try:
-        loop.run_forever()
-    finally:
-        loop.close()
+    asyncio.run(main())

bb81db1a568ca469ae3aa46383479bf03b033c4e
Kirill K <kovalev.kirill.a@gmail.com>
2022-07-26 14:59:39 +0300

    feat: add consumer add

---
+++ app_consumer_add.py
@@ -0,0 +1,98 @@
+import gspread
+import logging
+import nats
+import os
+import pickle
+
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
+
+from tg_step_counter.i18n import Internationalization as I18n
+
+from tg_step_counter.message_parser import MessageParser
+
+from tg_step_counter.objects.result import Result
+from tg_step_counter.objects.tg_user import TGUser, TGUserSpreadsheetHandler
+
+
+nats_address = os.environ.get("APP_NATS_ADDRESS", "nats://nats.nats.svc:4222")
+nats_subject = os.environ.get("APP_NATS_SUBJECT", "add.>")
+
+bot_token = os.environ.get("APP_TG_TOKEN")
+
+google_service_account_fname = os.environ.get("APP_GOOGLE_SA_PATH", "./config/google-service-account.json")
+google_sheet_uri = os.environ.get("APP_GOOGLE_SHEET_URI")
+
+app_language = os.environ.get("APP_LANG", "en")
+i18n = I18n(lang=app_language)
+
+
+async def handler(message, sheet):
+    logging.warning(f"Received a message on: {message.subject}")
+    data = pickle.loads(message.data)
+
+    logging.debug(data)
+
+    logging.warning("Creating bot")
+    bot = AsyncTeleBot(bot_token, parse_mode="Markdown")
+    message_parser = MessageParser()
+
+    try:
+        value, date = message_parser.parse_add_message(data.text)
+    except ValueError:
+        await bot.reply_to(data, "{add_parse_error}".format(**i18n.lang_map))
+        return None
+
+    result = Result(date_notation=date, value=value)
+
+    user_alias = data.from_user.username or f"{data.from_user.first_name} {data.from_user.last_name}"
+
+    tg_user = TGUser(id=data.from_user.id, alias=user_alias)
+    tg_user_handler = TGUserSpreadsheetHandler(sheet, tg_user)
+
+    tg_user_handler.touch()
+
+    try:
+        tg_user_handler.add_result(result)
+    except gspread.exceptions.APIError as e:
+        logging.error(f"Could not write results: {e}")
+        await bot.reply_to(data, "{webhook_error_write_results}".format(**i18n.lang_map))
+        return None
+
+    monthly_sum = sum(tg_user_handler.get_monthly_map(result.month).values())
+    monthly_sum_human = str(max(monthly_sum // 1000, 1)) + ",000"
+
+    await bot.reply_to(
+        data,
+        "{webhook_results_written}".format(**i18n.lang_map).format(**{"monthly_sum_human": monthly_sum_human}),
+    )
+
+    await bot.close_session()
+
+
+async def main():
+    logging.warning(f"Getting Google Spreadsheet: {google_sheet_uri}")
+    gc = gspread.service_account(filename=google_service_account_fname)
+    sheet = gc.open_by_url(google_sheet_uri).sheet1
+
+    logging.warning(f"Connecting to NATS at: {nats_address}")
+    async with (await nats.connect(nats_address)) as nc:
+        logging.warning(f"Getting updates for subject: {nats_subject}")
+        sub = await nc.subscribe(nats_subject)
+
+        while True:
+            try:
+                message = await sub.next_msg(timeout=60)
+                await handler(message, sheet)
+            except nats.errors.TimeoutError:
+                pass
+            except Exception as e:
+                logging.error(f"Error during handling message: {e}")
+
+        logging.warning("Moving past subscribe ...")
+
+
+if __name__ == "__main__":
+    logging.critical("Starting consumer/add")
+
+    asyncio.run(main())
--- tg_step_counter/message_parser.py
+++ tg_step_counter/message_parser.py
@@ -3,6 +3,19 @@ import re
 
 
 class MessageParser(object):
+    """
+    >>> mp = MessageParser()
+    >>> mp.get_value_from_reply('100')
+    100
+    >>> mp.get_date_from_notify('hey 31.12 hey')
+    '31.12'
+
+    >>> mp.parse_add_message('10000')
+    (10000, None)
+    >>> mp.parse_add_message('12000 31.12')
+    (12000, '31.12')
+    """
+
     def __init__(self):
         pass
 
@@ -27,3 +40,22 @@ class MessageParser(object):
             raise ValueError(f"Could not find date in message: {text}")
 
         return date
+
+    def parse_add_message(self, text):
+        text = text.strip()
+
+        try:
+            value = re.search(r"[1-9][0-9]*", text).group()
+            value = int(value)
+            logging.debug(f"Parsed value {value} from text: {text}")
+        except ValueError:
+            raise ValueError(f"Could not find value in message: {text}")
+
+        try:
+            date = re.search(r"[0-9]{2}\.[0-9]{2}", text).group()
+            logging.debug(f"Parsed date {date} from text: {text}")
+        except (ValueError, AttributeError):
+            logging.info(f"Could not find date in message: {text}")
+            date = None
+
+        return (value, date)
